d24d8d295307e0a640eb791bba22ed591d682a3f
gitpractice97 <khatrihitesh1995@yahoo.com>
2019-10-06 20:29:57 +0530

    git2

--- target/classes/static/js/ajaxlikedislike.js
+++ target/classes/static/js/ajaxlikedislike.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				alert("data submited")
 			}
 		});
-		$(this).html("Liked");
+		$(this).html("git");
 		$(this).siblings("button").html("Dislike");
 		$(this).siblings("button").attr("disabled", false);
 		$(this).attr("disabled", true);

cd474e063c828dbed738b2eb70507e96c0ef09fe
hiteshkhatri97 <hiteshkhatri97@gmail.com>
2019-10-06 21:12:14 +0530

    git 5

--- src/main/resources/static/js/ajaxlikedislike.js
+++ src/main/resources/static/js/ajaxlikedislike.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				alert("data submited")
 			}
 		});
-		$(this).html("test2");
+		$(this).html("demo");
 		$(this).siblings("button").html("Dislike");
 		$(this).siblings("button").attr("disabled", false);
 		$(this).attr("disabled", true);
